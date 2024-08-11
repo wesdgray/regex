53a27e1b0fa25499f81c53c7cebcd949fa2eab5d
Wes Gray <wesgray@gmail.com>
2024-08-10 22:00:53 -0700

    WIP: trying other approaches

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
 use core::fmt;
 use std::env;
 use std::io;
-use std::iter::Cloned;
 use std::process;
-use std::slice::Iter;
+
+#[derive(Debug, Copy)]
+struct RegularExpression {
+    alternations: Box<RegularExpression>,
+    tokens: Vec<RegexToken>,
+}
 
 #[derive(Debug, Clone)]
-enum RegularExpressionElement {
+enum RegexToken {
+    Prefix(PrefixMetaCharacter),
     Literal(char),
     Digit,
     Alphanumeric,
-    Star(Box<RegularExpressionElement>),
-    PositiveGroup(String),
-    NegativeGroup(String),
+    Postfix(PostfixMetaCharacter),
+    PositiveGroup(RegularExpression),
+    NegativeGroup(RegularExpression),
+}
+
+#[derive(Debug, Clone)]
+enum PrefixMetaCharacter {
+    Backslash,
+    Bracket,
+    Brace,
+    Parenthesis,
+}
+
+#[derive(Debug, Clone)]
+enum PostfixMetaCharacter {
+    Plus(Box<RegularExpression>),
+    Star(Box<RegularExpression>),
+}
+
+fn match_one(input_char: char, pattern: RegularExpression) -> bool {
+    use RegularExpression::*;
+    return match pattern {
+        Literal(c) => input_char == c,
+        Digit => input_char.is_ascii_digit(),
+        Alphanumeric => input_char.is_alphanumeric(),
+        _ => false,
+    }
 }
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     let p = compile_pattern(pattern).unwrap();
     println!("{:?}", p);
     let mut c = input_line.chars().peekable();
-    if let Some(RegularExpressionElement::Literal('^')) = p.first() {
+    if let Some(RegularExpression::Literal('^')) = p.first() {
         let mut p_iter = p.iter().map(Clone::clone).peekable();
         p_iter.next();
         return match_here(c, p_iter)
@@ -52,15 +81,64 @@ impl fmt::Display for RegexError {
     }
 }
 
-fn compile_pattern(pattern: &str) -> Result<Vec<RegularExpressionElement>, RegexError> {
-    use RegularExpressionElement::*;
-    let mut compiled: Vec<RegularExpressionElement> = Vec::new();
+fn compile_one(c: char) -> RegularExpression {
+    use RegularExpression::*;
+    use PrefixMetaCharacter::*;
+    return match c {
+        '\\' => Escape(Backslash),
+        '[' => Escape(Bracket),
+        '{' => Escape(Brace),
+        '(' => Escape(Parenthesis), 
+        c => Literal(c),
+    }
+}
+
+fn compile_esc(c: char) -> Result<RegularExpression, RegexError> {
+    use RegularExpression::*;
+    let token =  match c {
+        'd' => Digit,
+        'w' => Alphanumeric,
+        _ => return Err(RegexError::PatternError),
+    };
+    Ok(token)
+}
+
+fn compile_pattern2(pattern: &str) -> Result<Vec<RegularExpression>, RegexError> {
+    use RegularExpression::*;
+    use PrefixMetaCharacter::*;
+    let mut compiled: Vec<RegularExpression> = Vec::new();
+    let mut chars = pattern.chars().peekable();
+    while let (Some(cur), next) = (chars.next(), chars.peek()) {
+        let re = compile_one(cur);
+        let token = match (re, next) {
+            (Escape(_), None) => return Err(RegexError::PatternError),
+            (Escape(Backslash), Some(second)) => match compile_one(*second) {
+
+                // Plus | Star => Literal(*second),
+                _ => return Err(RegexError::PatternError), 
+            },
+            (re, Some('*')) => Star(Box::new(re)),
+            (Escape(Parenthesis), Some('^')) => {
+                NegativeGroup(String::new())
+            },
+            (Escape(Parenthesis), Some(_)) => {
+                PositiveGroup(String::new())
+            },
+        };
+        compiled.push(token);
+        chars.next();
+    }
+    Ok(compiled)
+}
+fn compile_pattern(pattern: &str) -> Result<Vec<RegularExpression>, RegexError> {
+    use RegularExpression::*;
+    let mut compiled: Vec<RegularExpression> = Vec::new();
     let mut chars = pattern.chars().peekable();
     while let (Some(cur), next) = (chars.next(), chars.peek()) {
         let token = match (cur, next) {
             ('*', Some('*')) => {
                 return Err(RegexError::PatternError);
-            }
+            },
             ('[', Some('^')) => {
                 let group_members: String = chars.by_ref()
                      .take_while(|x| *x != ']')
@@ -89,16 +167,61 @@ fn compile_pattern(pattern: &str) -> Result<Vec<RegularExpressionElement>, Regex
     Ok(compiled)
 }
 
-fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peekable<impl Iterator<Item = RegularExpressionElement>>) -> bool {
-    use RegularExpressionElement::*;
+fn compile_pattern3(pattern: &str) -> Result<Vec<RegularExpression>, RegexError> {
+    use RegularExpression::*;
+    let mut compiled: Vec<RegularExpression> = Vec::new();
+    let mut chars = pattern.chars().peekable();
+    while let (Some(cur), next) = (chars.next(), chars.peek()) {
+        let token = match cur {
+            '\\' => match next {
+                Some('\\') => return Err(RegexError::PatternError),
+                Some('d') => {
+                    chars.next();
+                    Digit
+                },
+                Some('w') => {
+                    chars.next();
+                    Alphanumeric
+                },
+            },
+            '[' => match next {
+                Some('^') => {
+                    let group_members: String = chars.by_ref()
+                        .take_while(|x| *x != ']')
+                        .collect();
+                    NegativeGroup(group_members)
+                },
+                
+            }
+            ('*', Some('*')) => {
+                return Err(RegexError::PatternError);
+            },
+            ('[', Some('^')) => {
+            },
+            ('[', _) => {
+                let group_members: String = chars.by_ref()
+                     .take_while(|x| *x != ']')
+                     .collect();
+                PositiveGroup(group_members)
+            },
+            (c, _) => Literal(c),
+            
+        };
+        compiled.push(token);
+    }
+    Ok(compiled)
+}
+
+fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peekable<impl Iterator<Item = RegularExpression>>) -> bool {
+    use RegularExpression::*;
     return match (input.next(), regex.peek()) {
         (None, Some(Literal('$'))) => true,
         (_, None) => true, 
         (None, _) => false,
-        (Some(one_input), Some(re)) => {
+        (Some(input_char), Some(re)) => {
             match re {
                 Literal(c) => {
-                    if *c == one_input {
+                    if *c == input_char {
                         regex.next();
                         return match_here(input, regex);
                     } else {
@@ -106,7 +229,7 @@ fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peeka
                     }
                 },
                 Digit => {
-                    if one_input.is_ascii_digit() {
+                    if input_char.is_ascii_digit() {
                         regex.next();
                         return match_here(input, regex);
                     } else {
@@ -114,7 +237,7 @@ fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peeka
                     }
                 },
                 Alphanumeric => {
-                    if one_input.is_alphanumeric() {
+                    if input_char.is_alphanumeric() {
                         regex.next();
                         return match_here(input, regex);
                     } else {
@@ -122,7 +245,7 @@ fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peeka
                     }
                 },
                 PositiveGroup(members) => {
-                    if members.contains(one_input) {
+                    if members.contains(input_char) {
                         regex.next();
                         return match_here(input, regex);
                     } else {
@@ -130,11 +253,28 @@ fn match_here(mut input: impl Iterator<Item = char>, mut regex: std::iter::Peeka
                     }
                 },
                 NegativeGroup(members) => {
-                    if !members.contains(one_input) {
+                    if !members.contains(input_char) {
                         regex.next();
                         return match_here(input, regex);
                     } else {
                         return false;
+                    }
+                },
+                Plus(plus) => {
+                    if !match_one(input_char, *plus.clone()){
+                        return false;
+                    } else {
+                        let mut input_char = input_char;
+                        loop {
+                            if !match_one(input_char, *plus.clone()){
+                                regex.next();
+                                return true;
+                            }
+                            input_char = match input.next() {
+                                Some(c) => c,
+                                None => {return true;}
+                            }
+                        }
                     }
                 }
                 _ => false
